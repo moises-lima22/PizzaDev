20d998bd0e7d1a4b952eb883a188c34fd336f5f2
Moises Silva <moises.silva2015@outlook.com>
2021-11-03 17:33:59 -0300

    initial commit

---
+++ SitePizzaDev/app.js
@@ -0,0 +1,80 @@
+//incrementa
+
+var botoesDecrementa = document.querySelectorAll(".btn-decrementa");
+console.log(botoesDecrementa)
+
+for(let botao of botoesDecrementa)
+{
+    botao.addEventListener('click', decrementa);
+    
+    function decrementa()
+    {
+    
+        var item = botao.closest(".item");
+        var input = item.querySelector(".quantidade");
+
+        if(input.value > 0)
+        {
+            input.value--;
+            var preco = pegaPrecoItem(item);
+            adicionarAototal(-preco);
+        }
+    }
+
+}
+
+var botoesIncrementa = document.querySelectorAll(".btn-incrementa");
+
+for(let botao of botoesIncrementa)
+{
+    botao.addEventListener('click', incrementa);
+
+    function incrementa()
+    {
+        var item = botao.closest(".item");
+
+        var input = item.querySelector(".quantidade");
+        input.value++;
+        
+        var preco = pegaPrecoItem(item);
+        
+        adicionarAototal(preco);
+    
+    }
+}
+
+var formPedido = document.forms.pedido;
+
+formPedido.addEventListener('submit', function(event)
+{
+    
+    var contador = 0;
+
+    var inputs = formPedido.querySelectorAll("input.quantidade");
+
+    for(let input of inputs)
+    {
+        if(input.value > 0)
+        {
+            contador++;
+        }
+    }
+
+    if(contador == 0)
+    {
+        alert("Deve ter pelo menos 1 pizza no pedido")
+        event.preventDefault();
+    }
+});
+
+function pegaPrecoItem(item){
+    var precoItem = item.querySelector(".preco-item");
+    return Number(precoItem.textContent);
+}
+
+function adicionarAototal(valor){
+    var elementoTotal = document.querySelector("#total");
+    elementoTotal.textContent = valor + Number(elementoTotal.textContent);
+    
+}
+
